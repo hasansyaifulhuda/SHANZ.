ececeb261827ac1e74c2e67aa91d28bd1a5ca1fe
hasansyaifulhuda <hasansyaifulhuda24@gmail.com>
2026-04-21 18:58:57 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -6,12 +6,23 @@ const data = [
       {
         name: "ARENA AI",
         url: "https://arena.ai/",
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=arena.ai"
-        },
-     {
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://arena.ai/"
+      },
+      {
         name: "CHATGPT",
         url: "https://chatgpt.com/",
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://chatgpt.com/"
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com"
+      },
+      {
+        name: "GEMINI",
+        url: "https://gemini.google.com/app",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://gemini.google.com/app"
+      },
+
+      {
+        name: "PERPLEXITY",
+        url: "https://www.perplexity.ai/",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://www.perplexity.ai/"
       }
     ]
   },
@@ -20,14 +31,9 @@ const data = [
     category: "FILM",
     items: [
       {
-        name: "SPOTIFY",
-        url: "https://open.spotify.com/intl-id/",
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=spotify.com"
-        },
-      {
         name: "MOVIEBOX",
         url: "https://themoviebox.xyz/",
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://themoviebox.xyz/"
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=themoviebox.xyz"
       },
       {
         name: "LAYARKACA21",
@@ -36,9 +42,37 @@ const data = [
       },
       {
         name: "PUSATFILM21",
-        url: "https://v3.pusatfilm21info.com/?v=d2af9e097fe9cf2a",
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://v3.pusatfilm21info.com/?v=d2af9e097fe9cf2a"
+        url: "https://v3.pusatfilm21info.com/",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://v3.pusatfilm21info.com/"
+      },
+      {
+        name: "IDLIX",
+        url: "https://z1.idlixku.com/",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://z1.idlixku.com/"
+      }
+    ]
+  },
+
+  {
+    category: "MUSIK",
+    items: [
+      {
+        name: "SPOTIFY",
+        url: "https://open.spotify.com/",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=spotify.com"
+      },
+    ]
+  },
+
+  {
+    category: "TOOLS",
+    items: [
+      {
+        name: "ILOVEIMG",
+        url: "https://www.iloveimg.com/",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=https://www.iloveimg.com/"
       },
     ]
   }
+
 ];
